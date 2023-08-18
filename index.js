e3a7bcd5d367fe3b30e997e5869fc4c99fbbaf48
fahim-khandakar <fahimkhandakar01@gmail.com>
2023-08-18 19:33:23 +0600

    added some functionality

--- index.js
+++ index.js
@@ -1,27 +1,58 @@
 function calculateTriangleArea() {
-  const baseField = document.getElementById("triangle-base");
-  const baseValueText = baseField.value;
-  const base = parseFloat(baseValueText);
-
-  const heightField = document.getElementById("triangle-height");
-  const heightValueText = heightField.value;
-  const height = parseFloat(heightValueText);
-
-  const area = 0.5 * base * height;
-  const areaSpan = document.getElementById("triangle-area-show");
-  areaSpan.innerText = area;
+  const base = getInputValue("triangle-base");
+  const height = getInputValue("triangle-height");
+  if (isNaN(base) || isNaN(height)) {
+    alert("Please provide me number");
+    return;
+  }
+  const value = 0.5 * base * height;
+  setValue("triangle-area-show");
 }
 
 function calculateRectangleArea() {
-  const widthField = document.getElementById("rectangle-width");
-  const widthValueText = widthField.value;
-  const width = parseFloat(widthValueText);
+  const width = getInputValue("rectangle-width");
+  const height = getInputValue("rectangle-length");
+  if (isNaN(width) || isNaN(height)) {
+    alert("Please provide me number");
+    return;
+  }
+  const value = width * height;
+  setValue("rectangle-area-show", value);
+}
 
-  const lengthField = document.getElementById("rectangle-length");
-  const lengthValueText = lengthField.value;
-  const length = parseFloat(lengthValueText);
+function calculateParallelogramArea() {
+  const base = getInputValue("parallelogram-base");
+  const height = getInputValue("parallelogram-height");
+  if (isNaN(base) || isNaN(height)) {
+    alert("Please provide me number");
+    return;
+  }
+  const value = base * height;
+  setValue("parallelogram-area-show", value);
+}
 
-  const area = width * length;
-  const areaSpan = document.getElementById("rectangle-area-show");
-  areaSpan.innerText = area;
+function calculateEllipseArea() {
+  const majorRadius = getInputValue("ellipse-major-radius");
+  const minorRadius = getInputValue("ellipse-minor-radius");
+  if (isNaN(majorRadius) || isNaN(minorRadius)) {
+    alert("Please provide me number");
+    return;
+  }
+  const value = 3.14 * majorRadius * minorRadius;
+  const valueTwoDecimal = value.toFixed(2);
+  setValue("ellipse-area-show", valueTwoDecimal);
+}
+
+// reusable function
+function getInputValue(inputId) {
+  const inputField = document.getElementById(inputId);
+  const inputValueText = inputField.value;
+  const value = parseFloat(inputValueText);
+  return value;
+}
+
+// reusable set function
+function setValue(outputId, value) {
+  const getId = document.getElementById(outputId);
+  getId.innerText = value;
 }
